91b88d9f2b74ca69c5a58a317420b9784bfbb9ae
Sam Hildebrand <sch_numberone@yahoo.com>
2022-11-08 15:46:46 -0600

    more nav stuffs

--- lib/nav_bar.dart
+++ lib/nav_bar.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'svgs.dart' as svgs;
 
 class NavBar extends StatelessWidget {
   const NavBar({super.key});
@@ -6,8 +8,22 @@ class NavBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Row(
-      children: const [
-        Text("lolololol"),
+      children: [
+        SvgPicture.string(
+          svgs.blueHouse,
+          allowDrawingOutsideViewBox: true,
+          fit: BoxFit.fill,
+        ),
+        const Text(
+          'Home',
+          style: TextStyle(
+            fontFamily: 'SF Pro',
+            fontSize: 18,
+            color: Color(0xff2e9eff),
+            fontWeight: FontWeight.w800,
+          ),
+          softWrap: false,
+        ),
       ],
     );
   }
