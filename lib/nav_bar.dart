6d5f3b0772227ee018899920620cee0c9af36ab2
maxreulet <mreule2@lsu.edu>
2022-11-10 16:14:32 -0600

    maxs nav bar

--- lib/nav_bar.dart
+++ lib/nav_bar.dart
@@ -2,13 +2,56 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'svgs.dart' as svgs;
 
-class NavBar extends StatelessWidget {
+class NavBar extends StatefulWidget {
   const NavBar({super.key});
 
   @override
+  State<NavBar> createState() => _NavBar();
+}
+
+class _NavBar extends State<NavBar> {
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState (() {
+    _selectedIndex = index;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 0.0, vertical: 10.0),
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        items: const <BottomNavigationBarItem> [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home_outlined),
+            label: 'Home' ,
+            ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.explore_outlined),
+            label: 'Search' ,
+            ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.sports_volleyball_outlined),
+            label: 'Reservations' ,
+            ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.messenger_outline),
+            label: 'Messages' ,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person_outline),
+            label: 'Profile',
+            )
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Color(0xff2e9eff),
+        onTap: _onItemTapped,
+      )
+      ); 
+    /* return Padding(
+         padding: const EdgeInsets.symmetric(horizontal: 0.0, vertical: 10.0),
         child: Row(
           children: [
             SizedBox(
@@ -31,6 +74,6 @@ class NavBar extends StatelessWidget {
               ),
             )
           ],
-        ));
+        )); */
   }
 }
