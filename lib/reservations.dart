2be25c5db44fb8bdcefb9ee208621068280ccfa9
Sam Hildebrand <sch_numberone@yahoo.com>
2022-11-21 17:30:17 -0600

    fixes

--- lib/reservations.dart
+++ lib/reservations.dart
@@ -3,17 +3,38 @@ import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 
-class Reservations extends StatelessWidget {
-  const Reservations({super.key});
+class Reservations extends StatefulWidget {
+  const Reservations({Key? key}) : super(key: key);
+
+  @override
+  State<Reservations> createState() => _Reservations();
+}
+
+class _Reservations extends State<Reservations> {
+  Future<QuerySnapshot>? searchResults;
+  String searchQuery = '';
+
+  initSearchLisiting(String textEntered) {
+    searchResults = FirebaseFirestore.instance
+        .collection('Listing')
+        .where('sport', isGreaterThanOrEqualTo: textEntered)
+        .get();
+
+    setState(() {
+      searchResults;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Stack(children: const <Widget>[
-          Text(""),
-        ]),
-      ),
+      body: FutureBuilder<QuerySnapshot>(
+          future: searchResults,
+          builder: (context, snapshot) {
+            return Center(
+              child: Stack(children: <Widget>[Text(searchResults.toString())]),
+            );
+          }),
     );
   }
 }
