220d1c59a4f40bd3306adc129226e8d0f2873ac2
maxreulet <mreule2@lsu.edu>
2022-11-21 14:36:59 -0600

    res screen

--- lib/reservations_screen.dart
+++ lib/reservations_screen.dart
@@ -22,9 +22,11 @@ class _ReservationsScreenState extends State<ReservationsScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
+      body: Column(
         children: <Widget>[
-              const Align(
+              Row(
+                children: [
+                Align(
                 alignment: Alignment.topLeft,
                 child: Padding(
                   padding: EdgeInsets.fromLTRB(10, 30, 0, 0),
@@ -42,17 +44,26 @@ class _ReservationsScreenState extends State<ReservationsScreen> {
               Align(
                 alignment: Alignment.topRight,
                 child: Padding(
-                  padding: EdgeInsets.fromLTRB(0, 40, 10, 0),
+                  padding: EdgeInsets.fromLTRB(160, 30, 10, 0),
                   child: SvgPicture.string(
                     svgs.notificationBell,
                   ),
                   ),
               ),
+        ],
+      ),
               Padding(
-                padding: const EdgeInsets.all(18.0),
+                padding: const EdgeInsets.all(10),
+                child: Container(
+                  width: 420,
+                  height: 330,
                 child: Card(
+                  shape: RoundedRectangleBorder(  
+                  borderRadius: BorderRadius.circular(10.0),  
+                 ),  
                   color: Colors.white,
                   child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
                     children: [
                       ListTile(
                         title: Text(
@@ -65,10 +76,64 @@ class _ReservationsScreenState extends State<ReservationsScreen> {
                         fontWeight: FontWeight.w800),
                   ),
                   ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 0, left: 10, right: 10),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(20),
+                    child: Image.asset('assets/listings-pictures/baseball-field-image2.jpeg'),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 10),
+                      child: Text(
+                        "Traction Sports Complex",
+                        style: TextStyle(
+                        fontFamily: 'SF Pro',
+                        fontSize: 20,
+                        color: Color(0xff000000),
+                        fontWeight: FontWeight.w800),
+                        ),
+                      ),
+                  Row( 
+                  children: [
+                    Align(
+                      alignment: Alignment.topLeft,
+                      child: Padding(
+                      padding: EdgeInsets.only(left: 10),
+                      child: SvgPicture.string(
+                        svgs.mapPin,
+                  ),
+                  ),
+               ),
+               Padding(
+                padding: EdgeInsets.only(left:10 ,bottom: 2),
+                child : Text(
+                  "1430 Gardere Ln, Baton Rouge, LA 70820",
+                  style: TextStyle(
+                    fontFamily: 'SF Pro',
+                    fontSize: 16,
+                    color: Color(0xff000000),
+                    fontWeight: FontWeight.w400),
+                  ),
+                ),
+                    ],
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(left:10,bottom:1),
+                    child: Text(
+                      "\$250 / hour",
+                      style:TextStyle(
+                      fontFamily: 'SF Pro',
+                     fontSize: 16,
+                      color: Color(0xff000000),
+                      fontWeight: FontWeight.w800),
+                    ),
+                    ),
                   ],
                   ),
                 ),
-                ),
+              ),
+              ),
         ],
       ),
       bottomNavigationBar: const NavBar(),
